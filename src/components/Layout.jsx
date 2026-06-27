d8b6568f539e2c34adefcb678a83431ca46d16a2
Karl97490 <karl.pery@icloud.com>
2026-06-27 11:22:25 +0400

    add dashboard and sidebar

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,10 +1,12 @@
 import { Outlet } from "react-router-dom";
 import Navbar from "./Navbar";
+import SideBar from "./SideBar";
 
 const Layout = () => {
   return (
     <div className="min-h-screen flex flex-col">
       <Navbar />
+      <SideBar />
 
       <main className="flex-1 container mx-auto px-4 py-0 place-content-center">
         <Outlet />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -21,132 +21,113 @@ const Navbar = () => {
     navigate("/login");
   };
 
-  return (
-    <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
-      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <NavLink
-          to="/"
-          className="flex items-center space-x-3 rtl:space-x-reverse"
-        >
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-7"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">
-            Flowbite
-          </span>
-        </NavLink>
-        <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-          {isLoggedIn ? (
-            <>
-              <button
-                type="button"
-                className="flex text-sm bg-neutral-primary rounded-full md:me-0 focus:ring-4 focus:ring-neutral-tertiary"
-                id="user-menu-button"
-                aria-expanded="false"
-                data-dropdown-toggle="user-dropdown"
-                data-dropdown-placement="bottom"
+  if (!isLoggedIn) {
+    return (
+      <nav className="bg-neutral-primary fixed w-full z-20 top-0 start-0 border-b border-default">
+        <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+          <NavLink
+            to="/"
+            className="flex items-center space-x-3 rtl:space-x-reverse"
+          >
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="h-7"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center text-xl text-heading font-semibold whitespace-nowrap">
+              Flowbite
+            </span>
+          </NavLink>
+          <div className="inline-flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+            <NavLink
+              to="/login"
+              className="text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none"
+            >
+              Get started
+            </NavLink>
+            <button
+              data-collapse-toggle="navbar-cta"
+              type="button"
+              className="inline-flex items-center p-2 w-9 h-9 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
+              aria-controls="navbar-cta"
+              aria-expanded="false"
+            >
+              <span className="sr-only">Open main menu</span>
+              <svg
+                className="w-6 h-6"
+                aria-hidden="true"
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                fill="none"
+                viewBox="0 0 24 24"
               >
-                <span className="sr-only">Open user menu</span>
-                <img
-                  className="w-8 h-8 rounded-full"
-                  src="/docs/images/people/profile-picture-5.jpg"
-                  alt="user photo"
+                <path
+                  stroke="currentColor"
+                  strokeLinecap="round"
+                  strokeWidth="2"
+                  d="M5 7h14M5 12h14M5 17h14"
                 />
-              </button>
-              {/* Dropdown user menu */}
-              <div
-                className="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44"
-                id="user-dropdown"
-              >
-                <div className="px-4 py-3 text-sm border-b border-default">
-                  <span className="block text-heading font-medium">
-                    Joseph McFall
-                  </span>
-                  <span className="block text-body truncate">
-                    name@flowbite.com
-                  </span>
-                </div>
-                <ul
-                  className="p-2 text-sm text-body font-medium"
-                  aria-labelledby="user-menu-button"
+              </svg>
+            </button>
+          </div>
+          <div
+            className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+            id="navbar-cta"
+          >
+            <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-default rounded-base bg-neutral-secondary-soft md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 md:bg-neutral-primary">
+              <li>
+                <NavLink
+                  to="/"
+                  className="block py-2 px-3 text-white bg-brand rounded md:bg-transparent md:text-fg-brand md:p-0"
+                  aria-current="page"
                 >
-                  <li>
-                    <NavLink
-                      to="/profile"
-                      className="inline-flex items-center gap-x-1 w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                    >
-                      <CircleUserRound size={18} />
-                      Profile
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/dashboard"
-                      className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                    >
-                      Dashboard
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/clients"
-                      className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                    >
-                      Clients
-                    </NavLink>
-                  </li>
-                  <li>
-                    <NavLink
-                      to="/invoices"
-                      className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                    >
-                      Invoices
-                    </NavLink>
-                  </li>
-                  <li>
-                    <button
-                      onClick={handleLogOut}
-                      className="inline-flex gap-x-1 items-center w-full p-2 bg-gray-50 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
-                    >
-                      <svg
-                        className="w-4 h-4 text-gray-800 dark:text-white"
-                        aria-hidden="true"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 18 16"
-                      >
-                        <path
-                          stroke="currentColor"
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth="1"
-                          d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"
-                        />
-                      </svg>
-                      Sign out
-                    </button>
-                  </li>
-                </ul>
-              </div>
-            </>
-          ) : (
-            <>
-              <NavLink
-                to="/login"
-                className="text-white bg-brand hover:bg-brand-strong box-border border border-transparent focus:ring-4 focus:ring-brand-medium shadow-xs font-medium leading-5 rounded-base text-sm px-3 py-2 focus:outline-none"
-              >
-                Get started
-              </NavLink>
+                  Home
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/about"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  About
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/services"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Services
+                </NavLink>
+              </li>
+              <li>
+                <NavLink
+                  to="/contact"
+                  className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
+                >
+                  Contact
+                </NavLink>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    );
+  } else {
+    return (
+      <nav className="fixed top-0 z-50 w-full bg-neutral-primary-soft border-b border-default">
+        <div className="px-3 py-3 lg:px-5 lg:pl-3">
+          <div className="flex items-center justify-between">
+            <div className="flex items-center justify-start rtl:justify-end">
               <button
-                data-collapse-toggle="navbar-sticky"
+                data-drawer-target="top-bar-sidebar"
+                data-drawer-toggle="top-bar-sidebar"
+                aria-controls="top-bar-sidebar"
                 type="button"
-                className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-body rounded-base md:hidden hover:bg-neutral-secondary-soft hover:text-heading focus:outline-none focus:ring-2 focus:ring-neutral-tertiary"
-                aria-controls="navbar-sticky"
-                aria-expanded="false"
+                className="sm:hidden text-heading bg-transparent box-border border border-transparent hover:bg-neutral-secondary-medium focus:ring-4 focus:ring-neutral-tertiary font-medium leading-5 rounded-base text-sm p-2 focus:outline-none"
               >
-                <span className="sr-only">Open main menu</span>
+                <span className="sr-only">Open sidebar</span>
                 <svg
                   className="w-6 h-6"
                   aria-hidden="true"
@@ -160,56 +141,114 @@ const Navbar = () => {
                     stroke="currentColor"
                     strokeLinecap="round"
                     strokeWidth="2"
-                    d="M5 7h14M5 12h14M5 17h14"
+                    d="M5 7h14M5 12h14M5 17h10"
                   />
                 </svg>
               </button>
-            </>
-          )}
+              <a href="https://flowbite.com" className="flex ms-2 md:me-24">
+                <img
+                  src="https://flowbite.com/docs/images/logo.svg"
+                  className="h-6 me-3"
+                  alt="FlowBite Logo"
+                />
+                <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white">
+                  Flowbite
+                </span>
+              </a>
+            </div>
+            <div className="flex items-center">
+              <div className="flex items-center ms-3">
+                <div>
+                  <button
+                    type="button"
+                    className="flex text-sm bg-gray-800 rounded-full focus:ring-4 focus:ring-gray-300 dark:focus:ring-gray-600"
+                    aria-expanded="false"
+                    data-dropdown-toggle="dropdown-user"
+                  >
+                    <span className="sr-only">Open user menu</span>
+                    <img
+                      className="w-8 h-8 rounded-full"
+                      src="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+                      alt="user photo"
+                    />
+                  </button>
+                </div>
+                <div
+                  className="z-50 hidden bg-neutral-primary-medium border border-default-medium rounded-base shadow-lg w-44"
+                  id="dropdown-user"
+                >
+                  <div
+                    className="px-4 py-3 border-b border-default-medium"
+                    role="none"
+                  >
+                    <p className="text-sm font-medium text-heading" role="none">
+                      Neil Sims
+                    </p>
+                    <p className="text-sm text-body truncate" role="none">
+                      neil.sims@flowbite.com
+                    </p>
+                  </div>
+                  <ul className="p-2 text-sm text-body font-medium" role="none">
+                    <li>
+                      <a
+                        href="#"
+                        className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                        role="menuitem"
+                      >
+                        Dashboard
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                        role="menuitem"
+                      >
+                        Settings
+                      </a>
+                    </li>
+                    <li>
+                      <a
+                        href="#"
+                        className="inline-flex items-center w-full p-2 hover:bg-neutral-tertiary-medium hover:text-heading rounded"
+                        role="menuitem"
+                      >
+                        Earnings
+                      </a>
+                    </li>
+                    <li>
+                      <button
+                        className="inline-flex items-center gap-x-1 bg-red-50 text-red-400 w-full p-2 hover:bg-red-100 hover:text-red-500 rounded"
+                        role="menuitem"
+                        onClick={handleLogOut}
+                      >
+                        <svg
+                          className="shrink-0 w-4 h-4 transition duration-75 group-hover:text-red-500"
+                          aria-hidden="true"
+                          xmlns="http://www.w3.org/2000/svg"
+                          fill="none"
+                          viewBox="0 0 24 24"
+                        >
+                          <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M16 12H4m12 0-4 4m4-4-4-4m3-4h2a3 3 0 0 1 3 3v10a3 3 0 0 1-3 3h-2"
+                          />
+                        </svg>
+                        Sign out
+                      </button>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <div
-          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-          id="navbar-sticky"
-        >
-          <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-default rounded-base bg-neutral-secondary-soft md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-neutral-primary">
-            <li>
-              <NavLink
-                to="/"
-                className="block py-2 px-3 text-white bg-brand rounded-sm md:bg-transparent md:text-fg-brand md:p-0"
-                aria-current="page"
-              >
-                Home
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/about"
-                className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
-              >
-                About
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/services"
-                className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
-              >
-                Services
-              </NavLink>
-            </li>
-            <li>
-              <NavLink
-                to="/contact"
-                className="block py-2 px-3 text-heading rounded hover:bg-neutral-tertiary md:hover:bg-transparent md:border-0 md:hover:text-fg-brand md:p-0 md:dark:hover:bg-transparent"
-              >
-                Contact
-              </NavLink>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-  );
+      </nav>
+    );
+  }
 };
 
 export default Navbar;
